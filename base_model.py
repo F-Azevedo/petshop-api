d8736b70d067fc6057d067e554f96704af5ae9a6
Fernando Azevedo <fernando.azevedo.peres@gmail.com>
2022-06-13 22:24:07 -0300

    Criação de classes para api reconhecer um objeto JSON com parametros opcionais para o PUT

--- base_model.py
+++ base_model.py
@@ -1,10 +1,17 @@
 from pydantic import BaseModel
+from typing import Union
 
 
 class Person(BaseModel):
     name: str
     document: int
-    date_of_birth: str
+    dateOfBirth: str
+
+
+class OptionalPerson(BaseModel):
+    name: Union[str, None] = None
+    document: Union[int, None] = None
+    dateOfBirth: Union[str, None] = None
 
 
 class Animal(BaseModel):
@@ -12,6 +19,13 @@ class Animal(BaseModel):
     cost: float
     species: str
     owner_id: int
+
+
+class OptionalAnimal(BaseModel):
+    name: Union[str, None] = None
+    cost: Union[float, None] = None
+    species: Union[str, None] = None
+    owner_id: Union[int, None] = None
 
 
 class Species(BaseModel):
